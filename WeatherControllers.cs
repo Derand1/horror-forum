c0b4cd71419b08afab12b8367d0c22460f0f572a
Derand1 <137919249+Derand1@users.noreply.github.com>
2026-02-17 20:00:19 +0300

    Add files via upload

---
+++ WeatherControllers.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace  MyfirstCI1.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class WeatherController : ControllerBase
+{
+    [HttpGet(template:"WeatherForecast")]
+    [Produces("application/json")]
+    public WeatherForecast[] Get()
+    {
+        var forcast = Enumerable.Range(1, 5).Select(Index =>
+            new WeatherForecast(
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(Index)), TemperatureC: Random.Shared.Next(-20, 55))
+                ).ToArray();
+            return forcast;
+    }
+}
+
+public record WeatherForecast(DateOnly Date, int TemperatureC)
+{
+    public int TemperatureK = TemperatureC + 273;
+}
+
